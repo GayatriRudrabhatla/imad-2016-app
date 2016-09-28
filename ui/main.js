5ad38246c2cef4b34b2fa4ffaea6bb614ce04377
GayatriRudrabhatla <gayatrir3@gmail.com>
2016-09-28 16:10:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,27 +7,11 @@ element.innerHTML = 'Welcome..';
 //Moving the picture
 var pic = document.getElementById('madi');
 var marginLeft = 0;
-var marginRight = 0;
-var i = 0;
+
 function  move()
 {
-  while(i<=5)
-  {
-    alert("Entered");      
    marginLeft = marginLeft + 5;
    pic.style.maginLeft = marginLeft+'px';
-   i++;
-  }
-  while(5<i<10)
-  {
-      marginRight = marginRight + 5;
-      pic.style.marginRight = marginRight+'px';
-      i++;
-  }
-  if(i==10)
-  {
-      i = 0;
-  }
 }
 
 pic.onclick = function()
