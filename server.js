3c9e67fc40ac2d3691d8de510ab09b0cb5a91942
GayatriRudrabhatla <gayatrir3@gmail.com>
2016-09-29 20:18:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,6 +72,7 @@ var htmltemplate = `
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 //app.get('/:articleName', function(req,res){
  //   var articleName = req.params.articleName;
  //  res.send(createHtmlTemplate(articles[articleName])); 
