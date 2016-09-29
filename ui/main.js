e1ce67d8f4056c08df7f2361e7300b8aba0b38dc
GayatriRudrabhatla <gayatrir3@gmail.com>
2016-09-29 22:53:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ button.onclick = function()
         if(request.status === 200)
         {
             var counter = request.responseText;
-            alert(counter.toString());
+           // alert(counter.toString());
             var span = document.getElementById('count');
             span.innerHTML = counter.toString();
         }
