c955afc1836b36e0fe550ccda6e8afa4924eeadd
GayatriRudrabhatla <gayatrir3@gmail.com>
2016-09-29 22:37:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,21 +7,21 @@ button.onclick = function()
     var request = new XMLHttpRequest();
     
     //Capture the response
-  //  request.onreadystatechange = function()
-   // {
+    request.onreadystatechange = function()
+   {
         if(request.readyState() === XMLHttpRequest.DONE)
     {
         if(request.status === 200)
         {
-            var counter = request.responseText;
+            var counter = request.ResponseText;
             var span = document.getElementById('count');
             span.innerHTML = counter.toString();
         }
     }
     
-  //      };  
+        };  
     //Make Request
-    request.open('GET','http://gayatrirudrabhatla.imad.hasura-app.io/counter',true);
+    request.open('GET','http://gayatrirudrabhatla.imad.hasura-app.io/counter', true);
     requset.send(null);
     
 
